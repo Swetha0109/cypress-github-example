233cea78e586f27b0e512fa651a0c7de35a32fe6
Swetha0109 <swethabinduchundi@gmail.com>
2024-04-30 17:09:47 +0530

    new code updated

--- Cypress/e2e/LetCode.cy.js
+++ Cypress/e2e/LetCode.cy.js
@@ -1,17 +1,19 @@
-import {letcode} from "..//PageObject/LetCode";
+//import {letcode} from "..//PageObject/LetCode";
 describe('LetCode Signup', () => {
     //test scenario or test suit
     it('LetCode Signup Functionality', () => {
-        //cy.visit('https://letcode.in');
-        // cy.fixture('LetCode.json').then((el) => {
-        //     cy.get('[placeholder="Enter your name"]').type(el.name);
-        //     cy.get('[placeholder="Enter valid email address"]').type(el.email);
-        //     cy.get('[type="password"]').type(el.password);
-        //     cy.get('label.checkbox').click();
-        //     cy.get("button[class='button is-primary']").click();
+        cy.visit('https://letcode.in/signup');
+        const email=`bindu1${Math.floor(Math.random* 10000)}@gmail.com`;
+        const name=`bindu1${Math.floor(Math.random*1000)}`;
+        const password="swetha";
+        //cy.fixture('LetCode.json').then((el) => {
+            cy.get('#name').type(name);
+            cy.get('#email').type(email);
+            cy.get('#pass').type(password);
+            cy.get('.checkbox').click();
+            cy.get("button[class='button is-primary']").click();
         // })
-        //letcode.signupUserDetails();
-        letcode.signinUserDetails();
+        
     })
 
 
